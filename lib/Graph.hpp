c1bd660bd2fd54b7288507dbe0591c428b06d253
osakana11011 <taka.knk.11011@gmail.com>
2019-05-16 00:48:16 +0900

    modified Graph.cpp using template

--- lib/Graph.hpp
+++ lib/Graph.hpp
@@ -3,53 +3,57 @@
 
 using namespace std;
 
-typedef long long ll;
-typedef pair<ll , ll> P;
+// 型
+using ll = long long;
+template <typename T> using P = pair<T, T>;
+
+// 定数
 const ll INF = 1e18;
 
-// グラフの辺
+// 辺
+template <typename T>
 struct Edge {
   int to;
-  int cost;
-
-  Edge(int to, int cost) : to(to), cost(cost) {}
+  T cost;
+  Edge(int to, T cost) : to(to), cost(cost) {}
 };
 
 // ノード
+template <typename T>
 struct Node {
-  vector<Edge> edges;
-
+  vector< Edge<T> > edges;
   Node() {}
-  void addEdge(int to, int cost) {
-    edges.push_back(Edge(to, cost));
+  void addEdge(int to, T cost) {
+    edges.push_back(Edge<T>(to, cost));
   }
 };
 
 // グラフ
+template <typename T>
 class Graph {
   private:
     int nodeN;
-    vector<Node> graph;
+    vector< Node<T> > graph;
   public:
     Graph(int nodeN) {
       this->nodeN = nodeN;
       for(int i = 0; i < nodeN; i++) {
-        graph.push_back(Node());
+        graph.push_back(Node<T>());
       }
     }
     /**
       辺を追加する
       */
-    void addEdge(int from, int to, int cost) {
+    void addEdge(int from, int to, T cost) {
       graph[from].addEdge(to, cost);
     }
     /**
       ダイクストラ法
       s: 始点
       */
-    vector<ll> dijkstra(int s) {
+    vector<T> dijkstra(int s) {
       // 距離情報初期化
-      vector<ll> d;
+      vector<T> d;
       for(int i = 0; i < this->nodeN; i++) {
         d.push_back(INF);
       }
@@ -57,18 +61,19 @@ class Graph {
 
       // sから探索を始める
       // キューの要素は、(コスト, ノード番号)
-      priority_queue< P , vector<P> ,greater<P> > queue;
-      queue.push(P(0, s));
+      priority_queue< P<T> , vector< P<T> > ,greater< P<T> > > queue;
+      queue.push(P<T>(0, s));
       while(!queue.empty()) {
-        P p=queue.top(); queue.pop();
-        ll pos=p.second, cost=p.first;
+        P<T> p=queue.top(); queue.pop();
+        T cost=p.first;
+        int pos=p.second;
 
         if(d[pos] < cost) continue;
         for(int i = 0; i < this->graph[pos].edges.size(); i++){
-          Edge e = this->graph[pos].edges[i];
+          Edge<T> e = this->graph[pos].edges[i];
           if(cost + e.cost < d[e.to]) {
             d[e.to] = cost + e.cost;
-            queue.push(P(d[e.to], e.to));
+            queue.push(P<T>(d[e.to], e.to));
           }
         }
       }
