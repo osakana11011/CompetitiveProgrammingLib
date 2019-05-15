38e7e185939f9a0328819bd2b93b8758b11c6140
osakana11011 <taka.knk.11011@gmail.com>
2019-05-16 01:07:18 +0900

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,31 @@
+// Add compile option "-std=c++11" for C++11 compile.
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <algorithm>
+#include <math>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <regex>
+#include <numeric>
+using namespace std;
+
+// Type
+template <typename T> using P = pair<T, T>;
+using ll = long long;
+using ull = unsigned long long;
+
+// Constants
+const ll INF = 1e18;
+const ll MOD = 1000000007;
+
+
+int main() {
+
+  return 0;
+}
