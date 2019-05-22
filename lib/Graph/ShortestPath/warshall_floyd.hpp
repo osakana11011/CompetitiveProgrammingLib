c6464726485904707523d3948c0708d60bb579c1
osakana11011 <taka.knk.11011@gmail.com>
2019-05-22 17:18:19 +0900

    modified warshall_floyd

--- lib/Graph/ShortestPath/warshall_floyd.hpp
+++ lib/Graph/ShortestPath/warshall_floyd.hpp
@@ -1,20 +1,36 @@
 /**
-  ワーシャルフロイド法
-  計算量: O(V^3)
-  */
-vector<vector<ll>> warshallFloyd(Graph graph) {
+  ワーシャルフロイド法 (全点対最短経路問題)
+
+  引数:
+    - Graph<T> g: グラフ
+
+  返り値:
+    - 全ての対に関する最短経路リスト
+      ex) d[from][to]: fromからtoへの最短経路
+
+  計算量:
+    - O(V^3)
+
+  依存ライブラリ:
+    - template.hpp
+    - Graph/ShortestPath/common.hpp
+*/
+template <typename T = int>
+vector<vector<T>> warshallFloyd(Graph<T> g) {
   // 距離情報初期化
+  T INF = getINF<T>();
   int n = graph.getNodeN();
-  vector<vector<ll>> d(n, vector<ll>(n, INF));
+  vector<vector<T>> d(n, vector<T>(n, INF));
   for(int i = 0; i < n; i++) {
     d[i][i] = 0;
   }
-  for(Node node : graph.getNodes()) {
-    for(Edge edge : node.edges) {
-      d[node.id][edge.to] = edge.cost;
+  for(int from = 0; from < g.n; from++) {
+    for(Edge edge : g.graph[from]) {
+      d[from][edge.to] = edge.cost;
     }
   }
 
+  // i → k → jと進むルートについて全探索
   for(int k = 0; k < n; k++) {
     for(int i = 0; i < n; i++) {
       for(int j = 0; j < n; j++) {
