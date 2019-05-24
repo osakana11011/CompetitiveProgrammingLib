1fa95b3051393193b6539ff2cbfcd6443a7786d4
osakana11011 <taka.knk.11011@gmail.com>
2019-05-24 14:31:31 +0900

    modified kruskal

--- lib/Tree/kruskal.hpp
+++ lib/Tree/kruskal.hpp
@@ -1,24 +1,38 @@
-FIXME: Graphを修正したので直す
-ll kruskal(Graph graph) {
-  // 辺を全てpriority_queueに突っ込む
-  using P = pair<ll, pair<int, int>>;
-  priority_queue<P, vector<P>, greater<P>> queue;
-  for(Node node : graph.getNodes()) {
-    for(Edge edge : node.edges) {
-      queue.push(make_pair(edge.cost, make_pair(node.id, edge.to)));
+/**
+  クラスカル法 (最小全域木)
+
+  引数:
+    - Graph<T> g: グラフ
+
+  返り値:
+    - 最小全域木の総コスト
+
+  依存ライブラリ:
+    - template.hpp
+    - Graph/ShortestPath/common.hpp
+    - Tree/union_find.hpp
+*/
+template <typename T = int>
+T kruskal(Graph<T> g) {
+  // 全ての辺をpriority_queueに突っ込む
+  using _P = pair<T, P<int>>;  // <辺のコスト, <辺の始点, 辺の終点>>
+  priority_queue<_P, vector<_P>, greater<_P>> queue;
+  for(int from = 0; from < g.n; from++) {
+    for(Edge<T> edge : g.graph[from]) {
+      queue.push(make_pair(edge.cost, make_pair(from, edge.to)));
     }
   }
 
   // 辺を小さい順に見ていき、UnionFindで閉路検出しながら最小全域木を構築
-  int n = graph.getNodeN();
-  UnionFind uf = UnionFind(n);
-  sumTreeCost = 0;
+  UnionFind uf = UnionFind(g.n);
+  T sumTreeCost = 0;
   while(!queue.empty()) {
-    P p = queue.top(); queue.pop();
-    ll cost = p.first;
+    _P p = queue.top(); queue.pop();
+    T cost = p.first;
     int from = p.second.first, to = p.second.second;
-    if(uf.same(a, b)) continue;
-    uf.unite(a, b);
+
+    if(uf.same(to, from)) continue;
+    uf.unite(to, from);
     sumTreeCost += cost;
   }
   return sumTreeCost;
