74092aa9e7001cbc2e8a1fe18677791b946b03cf
osakana11011 <taka.knk.11011@gmail.com>
2019-05-26 20:41:14 +0900

    modified template

--- lib/template.hpp
+++ lib/template.hpp
@@ -16,22 +16,29 @@
 using namespace std;
 
 // Macro
-#define REP(i, N) for(int i=0; i<(int)N; i++)
-#define REPR(i, N) for(int i=(int)N-1; i>=0; i--)
+#define rep(i, N) for(int i=0; i<(int)N; i++)
+#define erep(i, N) for(int i=0; i<=(int)N; i++)
+#define mp make_pair
+#define pb push_back
 
 // Type
-using ll = long long;
-template <typename T = int> using P = pair<T, T>;
+typedef long long ll;
+typedef pair<int, int> Pi;
+typedef pair<ll, ll> Pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
 
 // Constants
-const ll MOD = 1000000007;
+const int MOD = (int)1e9 + 7;
+int dy[] = {0, 1, -1, 0};
+int dx[] = {1, 0, 0, -1};
 
 /**
   型に合わせたINFを返す
   int: 214,748,364 (2.14 * 1e8)
   ll : 922,337,203,685,477,580 (9.22 * 1e17)
 */
-template <typename T = int>
+template <typename T>
 T getINF() {
   // NOTE: INF同士を足してもオーバーフロー起こさないように、10で割っている
   return numeric_limits<T>::max() / 10;
@@ -41,3 +48,8 @@ T getINF() {
 const double EPS = 1e-11;
 #define EQ0(x) (abs(x) < EPS)
 #define EQ(x, y) (abs(a-b) < EPS)
+
+int main() {
+
+  return 0;
+}
